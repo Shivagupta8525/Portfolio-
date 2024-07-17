1eee1264bfeb2cb22651c73bfacc860d43f66ec3
CodeYogi <>
2024-07-17 22:45:21 +0530

    frist commit for portfolio

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// Dear CodeYogi Student, write your code here.
+ let hamburger = document.getElementById("hamburger");
+ hamburger.addEventListener('click',showLinks);
+ function showLinks(){
+   let dropdown = document.getElementById("dropdown");
+   if(dropdown.classList.contains("hidden")){
+     dropdown.classList.remove("hidden")
+   }
+   else{
+     dropdown.classList.add("hidden");
+   }
+ }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
    content: ["./index.html", "./app.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        darkorange:{
+          500:"#fc2a2a",
+        },
+        premium:{
+          500:"#21243D",
+        },
+        primary:{
+          500:"#FF6464",
+        }
+      },
+    },
   },
   plugins: [],
 }
